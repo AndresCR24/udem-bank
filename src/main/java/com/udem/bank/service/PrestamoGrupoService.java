815891be7fad60c7fd3cc11b901edd2063a98447
AndresCR24 <c-andres-98@hotmail.com>
2023-10-23 18:32:32 -0500

    se guarda para crear una nueva rama y hacer un test

--- src/main/java/com/udem/bank/service/PrestamoGrupoService.java
+++ src/main/java/com/udem/bank/service/PrestamoGrupoService.java
@@ -2,20 +2,28 @@ package com.udem.bank.service;
 
 import com.udem.bank.persistence.entity.GrupoAhorroEntity;
 import com.udem.bank.persistence.entity.PrestamoGrupoEntity;
+import com.udem.bank.persistence.entity.UsuarioEntity;
+import com.udem.bank.persistence.repository.GrupoAhorroRepository;
 import com.udem.bank.persistence.repository.PrestamoGrupoRepository;
+import com.udem.bank.persistence.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Service
 public class PrestamoGrupoService {
 
     private final PrestamoGrupoRepository prestamoGrupoRepository;
+    private final UsuarioRepository usuarioRepository;
+    private final GrupoAhorroRepository grupoAhorroRepository;
 
     @Autowired
-    public PrestamoGrupoService(PrestamoGrupoRepository prestamoGrupoRepository) {
+    public PrestamoGrupoService(PrestamoGrupoRepository prestamoGrupoRepository, UsuarioRepository usuarioRepository, GrupoAhorroRepository grupoAhorroRepository) {
         this.prestamoGrupoRepository = prestamoGrupoRepository;
+        this.usuarioRepository = usuarioRepository;
+        this.grupoAhorroRepository = grupoAhorroRepository;
     }
 
     //Devolver lista de cuentas
@@ -42,5 +50,35 @@ public class PrestamoGrupoService {
 
     public void deletePrestamoGrupo(int idPrestamoGrupo){
         this.prestamoGrupoRepository.deleteById(idPrestamoGrupo);
+    }
+
+    //Solicitar un prestamo a un grupo de ahorro
+    public PrestamoGrupoEntity solicitarPrestamo(Integer idUsuario, Integer idGrupo, BigDecimal monto) {
+        // Verificar si el usuario y el grupo existen
+        UsuarioEntity usuario = usuarioRepository.findById(idUsuario)
+                .orElseThrow(() -> new IllegalArgumentException("Usuario no encontrado"));
+        GrupoAhorroEntity grupo = grupoAhorroRepository.findById(idGrupo)
+                .orElseThrow(() -> new IllegalArgumentException("Grupo de ahorro no encontrado"));
+
+        // Validar si el usuario pertenece al grupo
+        if (!grupo.getUsuarios().contains(usuario)) {
+            throw new IllegalArgumentException("El usuario no pertenece al grupo de ahorro");
+        }
+
+        // Realizar otras validaciones según tus reglas de negocio
+
+        // Crear la entidad de préstamo
+        PrestamoGrupoEntity prestamo = new PrestamoGrupoEntity();
+        //prestamo.setUsuario(usuario);
+        prestamo.setGrupoAhorro(grupo);
+        prestamo.setMonto(monto);
+        //prestamo.setEstado("Pendiente"); // Puedes definir estados como "Pendiente", "Aprobado", "Rechazado", etc.
+
+        // Guardar la solicitud de préstamo en la base de datos
+        prestamoGrupoRepository.save(prestamo);
+
+        // Puedes realizar notificaciones aquí, por ejemplo, notificar al grupo sobre la solicitud.
+
+        return prestamo;
     }
 }
