0b573eb389dcfe2c3156f14988f7bafa376030c7
Cristian <crishzz01@gmail.com>
2023-11-08 21:38:06 -0500

    Test de usuario y cuenta de ahorros. Depedencias y mas

---
+++ src/test/java/com/udem/bank/ServiceTests/CuentaAhorrosServiceTest.java
@@ -0,0 +1,73 @@
+package com.udem.bank.ServiceTests;
+
+import com.udem.bank.persistence.entity.CuentaAhorrosEntity;
+import com.udem.bank.persistence.repository.CuentaAhorrosRepository;
+import com.udem.bank.service.CuentaAhorrosService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class CuentaAhorrosServiceTest {
+
+    @InjectMocks
+    private CuentaAhorrosService cuentaAhorrosService;
+
+    @Mock
+    private CuentaAhorrosRepository cuentaAhorrosRepository;
+
+    @Test
+    public void testGetAll() {
+        List<CuentaAhorrosEntity> cuentasAhorrosList = new ArrayList<>();
+        when(cuentaAhorrosRepository.findAll()).thenReturn(cuentasAhorrosList);
+
+        List<CuentaAhorrosEntity> result = cuentaAhorrosService.getAll();
+
+        assertEquals(cuentasAhorrosList, result);
+    }
+
+    @Test
+    public void testGet() {
+        CuentaAhorrosEntity cuentaAhorros = new CuentaAhorrosEntity();
+        when(cuentaAhorrosRepository.findById(1)).thenReturn(Optional.of(cuentaAhorros));
+
+        CuentaAhorrosEntity result = cuentaAhorrosService.get(1);
+
+        assertEquals(cuentaAhorros, result);
+    }
+
+    @Test
+    public void testSave() {
+        CuentaAhorrosEntity cuentaAhorros = new CuentaAhorrosEntity();
+        when(cuentaAhorrosRepository.save(cuentaAhorros)).thenReturn(cuentaAhorros);
+
+        CuentaAhorrosEntity result = cuentaAhorrosService.save(cuentaAhorros);
+
+        assertEquals(cuentaAhorros, result);
+    }
+
+    @Test
+    public void testExists() {
+        when(cuentaAhorrosRepository.existsById(1)).thenReturn(true);
+
+        boolean result = cuentaAhorrosService.exists(1);
+
+        assert(result);
+    }
+
+    @Test
+    public void testDeleteCuentaAhorros() {
+        cuentaAhorrosService.deleteCuentaAhorros(1);
+
+        verify(cuentaAhorrosRepository, times(1)).deleteById(1);
+    }
+}
---
+++ src/test/java/com/udem/bank/ServiceTests/UsuarioServiceTest.java
@@ -0,0 +1,78 @@
+package com.udem.bank.ServiceTests;
+import com.udem.bank.service.UsuarioService;
+
+import com.udem.bank.persistence.entity.UsuarioEntity;
+import com.udem.bank.persistence.repository.UsuarioRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UsuarioServiceTest {
+
+    @InjectMocks
+    private UsuarioService usuarioService;
+
+    @Mock
+    private UsuarioRepository usuarioRepository;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testGetAll() {
+        List<UsuarioEntity> usuariosList = new ArrayList<>();
+        when(usuarioRepository.findAll()).thenReturn(usuariosList);
+
+        List<UsuarioEntity> result = usuarioService.getAll();
+
+        assertEquals(usuariosList, result);
+    }
+
+    @Test
+    public void testGet() {
+        UsuarioEntity usuario = new UsuarioEntity();
+        when(usuarioRepository.findById(1)).thenReturn(Optional.of(usuario));
+
+        UsuarioEntity result = usuarioService.get(1);
+
+        assertEquals(usuario, result);
+    }
+
+    @Test
+    public void testSave() {
+        UsuarioEntity usuario = new UsuarioEntity();
+        when(usuarioRepository.save(usuario)).thenReturn(usuario);
+
+        UsuarioEntity result = usuarioService.save(usuario);
+
+        assertEquals(usuario, result);
+    }
+
+    @Test
+    public void testExists() {
+        when(usuarioRepository.existsById(1)).thenReturn(true);
+
+        boolean result = usuarioService.exists(1);
+
+        assert(result);
+    }
+
+    @Test
+    public void testDeleteUsuario() {
+        usuarioService.deleteUsuario(1);
+
+        verify(usuarioRepository, times(1)).deleteById(1);
+    }
+}
+
